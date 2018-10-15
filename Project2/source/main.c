8d5bc1a3a04232820ba6615a04ebd9d5f3d55ee6
Jacob Cassady <jtcass01@louisville.edu>
2018-10-15 05:18:22 +0000

    updated main.c to reflect calculator

--- Project2/source/main.c
+++ Project2/source/main.c
@@ -32,7 +32,6 @@ const char MS1[] = "\r\n\nCECS-525 RPI Tiny OS";
 const char MS2[] = "\r\nby Eugene Rockey Copyright 2013 All Rights Reserved";
 const char MS3[] = "\r\nReady: ";
 const char MS4[] = "\r\nInvalid Command Try Again...";
-const char CALCULATOR_MENU[] = "\r\n===== CALCULATOR MENU =====\nta) add\n\ts) subtract\n\tm) multiply\n\td) divide\n\tq) quit\n";
 const char GPUDATAERROR[] = "\r\nSystem Error: Invalid GPU Data";
 const char LOGONNAME[] = "eugene    ";
 const char PASSWORD[] = "cecs525   ";
@@ -121,323 +120,34 @@ uint8_t BCDtoUint8(uint8_t BCD)
 	return (BCD & 0x0F) + ((BCD >> 4) * 10);
 }
 
-void DATE(void)
-{
-	uart_puts("\r\nEnter DATE (S)et or (D)isplay\r\n");
-	uint8_t c = '\0';
-	while (c == '\0') 
-	{
-		c = uart_readc();
-	}
-	switch (c) {
-		case 'S' | 's':
-            
-            
-			
-            //Engineer the code here to Set the current Date in the DS3231M
-            
-            
-            
-            break;
-		case 'D' | 'd':
-			bcm2835_i2c_begin();
-			bcm2835_i2c_setClockDivider(BCM2835_I2C_CLOCK_DIVIDER_2500);
-			bcm2835_i2c_setSlaveAddress(0x68);
-			bcm2835_i2c_read(DOM,*buffer);
-			char ones = *buffer[0] & 0x0F;
-			char tens = (*buffer[0] >> 4);
-			uart_putc(tens+48);
-			uart_putc(ones+48);
-			bcm2835_i2c_read(MONTH,*buffer);
-			ones = *buffer[0] & 0x0F;
-			tens = (*buffer[0] >> 4);
-			uart_putc('/');
-			uart_putc(tens+48);
-			uart_putc(ones+48);
-			bcm2835_i2c_read(YEAR,*buffer);
-			ones = *buffer[0] & 0x0F;
-			tens = (*buffer[0] >> 4);
-			uart_putc('/');
-			uart_putc(tens+48);
-			uart_putc(ones+48);	
-			bcm2835_i2c_end();
-			break;
-		default:
-			uart_puts(MS4);
-			DATE();
-			break;
-	}
-}
-
-void TIME(void)
-{
-	uart_puts("\r\nType TIME (S)et or (D)isplay\r\n");
-	uint8_t c = '\0';
-	while (c == '\0') 
-	{
-		c = uart_readc();
-	}
-	switch (c) {
-		case 'S' | 's':
-			bcm2835_i2c_begin();
-			bcm2835_i2c_setClockDivider(BCM2835_I2C_CLOCK_DIVIDER_2500);
-			bcm2835_i2c_setSlaveAddress(0x68);
-			uart_puts("\r\nSet 24 Hour Time");
-			uart_puts("\r\nType Hours (two digits 00-23): ");
-			tens = ((uart_readc()-48) << 4) | 0x0F;
-			uart_putc((tens >> 4)+48);
-			ones = (uart_readc()-48) | 0xF0;
-			uart_putc((ones & 0x0F)+48);
-			if (BCDtoUint8(tens & ones) < 0 || BCDtoUint8(tens & ones) > 23) 
-			{
-				*buffer[0] = 0x00;
-				uart_puts("\r\nInvalid Hours Value!");
-				break;
-			}
-			else
-			{
-				*buffer[0] = tens & ones;
-			}			
-			bcm2835_i2c_write(HRS,*buffer);
-			uart_puts("\r\nType Minutes (two digits 00-59): ");
-			tens = ((uart_readc()-48) << 4) | 0x0F;
-			uart_putc((tens >> 4)+48);
-			ones = (uart_readc()-48) | 0xF0;
-			uart_putc((ones & 0x0F)+48);
-			if (BCDtoUint8(tens & ones) < 0 || BCDtoUint8(tens & ones) > 59) 
-			{
-				*buffer[0] = 0x00;
-				uart_puts("\r\nInvalid Minutes Value!");
-				break;
-			}
-			else
-			{
-				*buffer[0] = tens & ones;
-			}			
-			bcm2835_i2c_write(MINS,*buffer);			
-			uart_puts("\r\nType Seconds (two digits 00-59): ");
-			tens = ((uart_readc()-48) << 4) | 0x0F;
-			uart_putc((tens >> 4)+48);
-			ones = (uart_readc()-48) | 0xF0;
-			uart_putc((ones & 0x0F)+48);
-			if (BCDtoUint8(tens & ones) < 0 || BCDtoUint8(tens & ones) > 59) 
-			{
-				*buffer[0] = 0x00;
-				uart_puts("\r\nInvalid Seconds Value!");
-				break;
-			}
-			else
-			{
-				*buffer[0] = tens & ones;
-			}			
-			bcm2835_i2c_write(SECS,*buffer);			
-			uart_puts("\r\nTime is now set.");
-			bcm2835_i2c_end();
-			break;
-		case 'D' | 'd':
-            
-            
-            
-            
-			//Engineer the code here to Display the current Time stored in the DS3231M to HyperTerminal
-            
-            
-            
-            
-            
-			break;	
-		default:
-			uart_puts(MS4);
-			TIME();
-			break;	
-	}
-}
-
-void ALARM(void)
-{
-	uart_puts("\r\nType ALARM (S)et or (D)isplay or (T)est\r\n");
-	uint8_t c = '\0';
-	while (c == '\0') 
-	{
-		c = uart_readc();
-	}
-	switch (c) {
-		case 'S' | 's':
-			uart_puts("\r\nSet Seconds Alarm");
-			uart_puts("\r\nType Starting Alarm Seconds (two digits 05-59): ");
-			tens = ((uart_readc()-48) << 4) | 0x0F;
-			uart_putc((tens >> 4)+48);
-			ones = (uart_readc()-48) | 0xF0;
-			uart_putc((ones & 0x0F)+48);
-			if (BCDtoUint8(tens & ones) < 5 || BCDtoUint8(tens & ones) > 59) 
-			{
-				alarm[0] = 0x05;
-				uart_puts("\r\nInvalid Alarm Value, Value Reset to 5!");
-				break;
-			}
-			else
-			{
-				alarm[0] = tens & ones;
-				uart_puts("\r\nAlarm is now set.");
-			}
-			break;
-		case 'D' | 'd':
-			ones = alarm[0] & 0x0F;
-			tens = alarm[0] >> 4;
-			uart_putc(tens+48);
-			uart_putc(ones+48);
-			break;
-		case 'T' | 't':
-			if (BCDtoUint8(alarm[0]) < 5 || BCDtoUint8(alarm[0]) > 59)
-			{
-				uart_puts("\r\nAlarm Value Out of Range, Set to a Proper Value First!");
-				break;
-			}
-			uart_puts("\r\nPlease wait, now testing Alarm...");
-            
-            
-            
-            
-            
-            
-			//Engineer the code here to Drive the Pulse Width Modulated audio to the speaker jack.
-            
-            
-            
-            
-            
-			break;
-		default:
-			uart_puts(MS4);
-			ALARM();
-			break;	
-	}
-}
-
-void CANCOM(void)
-{
-	//Initialize SPI Peripheral I/O
-    bcm2835_spi_begin();
-    bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);      // The default
-    bcm2835_spi_setDataMode(BCM2835_SPI_MODE0);                   // The default
-    bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_65536); // The default
-    bcm2835_spi_chipSelect(BCM2835_SPI_CS0);                      // The default
-    bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW);      // the default
-    //Reset the CAN Controller
-	bcm2835_gpio_fsel(RPI_GPIO_P1_15, BCM2835_GPIO_FSEL_OUTP);//GPIO22 drives MCP2515 *RESET Pin low.
-	bcm2835_gpio_fsel(RPI_GPIO_P1_18, BCM2835_GPIO_FSEL_OUTP);//GPIO24 drives MCP2515 *CS Pin low
-	bcm2835_gpio_write(RPI_GPIO_P1_15, LOW);
-	bcm2835_delayMicroseconds(150);
-	bcm2835_gpio_write(RPI_GPIO_P1_15, HIGH);
-	
-	
-	uart_puts("\r\n(T)ransmit or (R)eceive\r\n"); //Choose to transmit or receive
-	uint8_t c = '\0';
-	while (c == '\0') 
-	{
-		c = uart_readc();
-	}
-	switch (c) {
-		case 'T' | 't':
-			bcm2835_gpio_write(RPI_GPIO_P1_18, LOW);
-			CANtransmit(0x30);
-			bcm2835_gpio_write(RPI_GPIO_P1_18, HIGH);
-			break;
-		case 'R' | 'r':
-			bcm2835_gpio_write(RPI_GPIO_P1_18, LOW);
-			uint8_t data = CANreceive();
-			bcm2835_gpio_write(RPI_GPIO_P1_18, HIGH);
-			break;
-		default:
-			uart_puts(MS4);
-			CANCOM;
-			break;
-	}
-	//Restore
-    bcm2835_gpio_fsel(RPI_GPIO_P1_15, BCM2835_GPIO_FSEL_INPT);//GPIO22 restored to input.
-	bcm2835_gpio_fsel(RPI_GPIO_P1_18, BCM2835_GPIO_FSEL_INPT);//GPIO24 restored to input.
-	bcm2835_spi_end();
-}
-
-void ADC(void) //Demo Analog to Digital Converter circuit
-{
-    
-    
-    
-    //Engineer the bare-metal code here to communicate with the MCP3008 ADC via the RPI SPI port
-    //This function should display on Hyperterminal the changing analog input on MCP3008 Channel 0 as a value from ~ 0 to 1023.
-    //The analog input could be from a potentiometer connected to 3.3V
-    //Use only 3.3V with the MCP3008 ADC chip circuit.
-    //See the ADC web link in helpful links doc.
-    
-    
-    
-    
-}
-
 void RES(void)
 {
 	reboot();
 }
 
-void HELP(void) //Command List
+void MENU(void) //Command List
 {
-	uart_puts("\r\n(A)DC,(C)ancom,(D)ate,(H)elp,a(L)arm,(R)eset,(S)FT,(T)ime,(V)FP11,(G)raded Demo");
+	uart_puts("\r\n(A)dd,(S)ubtract,(D)ivide,(M)ultiply");
 }
 
-void SFT(void) //Soft Floating Point Demo, optionally complete this command and related code for experience with floating point operations performed in software.
+void ADD(void)
 {
-	char a[20];
-	char b[20];
-	unsigned int c, d, result;
-	uart_puts("\r\nInput First Decimal Number eg. -0.04, 23.45, 1.89, -5.0");
-	//Get first input string here
-	uart_puts("\r\nInput Second Decimal Number eg. -0.04, 23.45, 1.89, -5.0");
-	//Get second input string here
-	c = ASCII_to_float32(a);
-	d = ASCII_to_float32(b);
-	uart_puts("\r\nResults...");
-	result = float32_add(c,d);
-	//print add result here
-	result = float32_sub(c,d);
-	//print sub result here
+	uart_puts("\r\nADD");
 }
 
-void VFP11(void) //ARM Vector Floating Point Unit Demo, see softfloat.c for some useful and helpful example code.
+void SUBTRACT(void)
 {
-    //This section is optional for bonus points.
-    //Engineer the VFP11 math coprocessor application here.
-    //Send a menu to hyperterminal as was done with the minimal computer
-    //FADD, FSUB, FMUL, and FDIV, and any other functions you wish to implement in ARM assembly routines.
-	
-
+	uart_puts("\r\nSUBTRACT");
 }
 
+void DIVIDE(void)
+{
+	uart_puts("\r\nDIVIDE");
+}
 
-
-void graded_demo(void){
-	uart_puts(CALCULATOR_MENU);
-
-	uint8_t c = '\0';
-        while (c == '\0') {
-		c = uart_readc();
-	}
-
-	switch(c) {
-		case 'A' | 'a':
-			uart_puts("\r\nAdding");
-			break;
-		case 'S' | 's':
-			uart_puts("\r\nSubtracting");
-			break;
-		case 'M' | 'm':
-			uart_puts("\r\nMultiplying");
-			break;
-		case 'D' | 'd':
-			uart_puts("\r\nDividing");
-			break;
-        }
-
+void MULTIPLY(void)
+{
+	uart_puts("\r\nMULTIPLY");
 }
 
 void command(void)
@@ -448,52 +158,23 @@ void command(void)
 		c = uart_readc();
 	}
 	switch (c) {
-		case 'C' | 'c':
-			CANCOM();
-			break;
-		case 'D' | 'd':
-			DATE();
-			break;
-		case 'T' | 't':
-			TIME();
-			break;
-		case 'L' | 'l':
-			ALARM();
-			break;
 		case 'A' | 'a':
-			ADC();
-			break;
-		case 'R' | 'r':
-			RES();
+			ADD();
 			break;
 		case 'S' | 's':
-			SFT();
+			SUBTRACT();
 			break;
-		case 'H' | 'h':
-			HELP();
+		case 'D' | 'd':
+			DIVIDE();
 			break;
-		case 'V' | 'v':
-			VFP11();
+		case 'M' | 'm':
+			MULTIPLY();
 			break;
-		case 'G' | 'g':
-			while(1) {graded_demo();}
-                        break;
 		default:
 			uart_puts(MS4);
-			HELP();
 			break;
 	}
-}
-
-int logon(void)
-{
-	int success = 0;
-    
-    
-	//Engineer the code here to LOGON to the system as was done with the MC68000 minimal computer.
-    
-    
-    return success;
+	MENU();
 }
 
 void kernel_main() 
@@ -502,18 +183,10 @@ void kernel_main()
 	enable_irq_57();
 	enable_arm_irq();
 
-//	if (logon() == 0) while (1) {}
 	banner();
-	HELP();
+	MENU();
 	while (1) {command();}
-	
-//	while (1)
-//	{
-//	uart_putc(' ');
-//	uart_putc('A');
-//	uart_putc(' ');
-//	testdelay();
-//	}
+
 }
 
 void irq_handler(void)
