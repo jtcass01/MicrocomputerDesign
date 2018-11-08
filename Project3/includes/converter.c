30e6e5c6fc6053bf116267d1d65123412859cbe7
Jacob Taylor Cassady <jtcass01@louisville.edu>
2018-11-07 20:39:16 -0500

    working on conversion

--- Project3/includes/converter.c
+++ Project3/includes/converter.c
@@ -25,10 +25,51 @@ void delete_single_precision_float(SinglePrecisionFloat *spf_float) {
   free(spf_float);
 }
 
+char *decimal_to_binary_c(float decimal) {
+	char binary_c[32], temp[32];
+	int index = 0, binary_size = 0;
+
+	// Initialize buffers
+	while (index < 32) {
+		*(binary_c + index) = '0';
+		*(temp + index) = '0';
+		index++;
+	}
+
+	index = 0;
+
+	// Store forward direction in temp.
+	while (decimal != 0 && index < 32) {
+		if ((decimal * 2) >= 1) { // No remainder
+			*(temp + index) = '1';
+			decimal *= 2;
+			decimal -= 1;
+		}
+		else {
+			*(temp + index) = '0';
+			decimal *= 2;
+		}
+		index++;
+		binary_size++;
+	}
+
+	//Reverese the direction
+	for (int i = 0; i < binary_size; i++) {
+		binary_c[i] = temp[binary_size - i - 1];
+	}
+
+	binary_c[binary_size] = '\0';
+
+	printf("Binary representation of right-half: %s\n", binary_c);
+
+	return binary_c;
+}
+
 char *int_to_binary_c(int integer) {
 	char binary_c[32], temp[32];
 	int index = 0, binary_size = 0;
 
+	// Initialize buffers
 	while(index < 32) {
 		*(binary_c + index) = '0';
 		*(temp + index) = '0';
@@ -37,11 +78,8 @@ char *int_to_binary_c(int integer) {
 
 	index = 0;
 
-	printf("Binary representation of left-half: %s\n", binary_c);
-
-
 	// Store forward direction in temp.
-	while(integer != 0) {
+	while(integer != 0 && index < 32) {
 		if((integer%2) == 0) { // No remainder
 			*(temp + index) = '0';
 		} else {
