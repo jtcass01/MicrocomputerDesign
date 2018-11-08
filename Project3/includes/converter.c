fe3811040d7f9a79ec968e10a088c3e34c470cef
Jacob Taylor Cassady <jtcass01@louisville.edu>
2018-11-08 00:17:10 -0500

    working on conversion

--- Project3/includes/converter.c
+++ Project3/includes/converter.c
@@ -265,7 +265,12 @@ int get_exponent(char *binary_left_half, int negative) {
 		printf("problem child: %s\n", binary_left_half);
 		while (binary_left_half[exponent_shift] != '\0') {
 			exponent_shift++;
+			if (binary_left_half[exponent_shift] == '1') {
+				break;
+			}
 		}
+
+		exponent_shift *= -1;
 			/*
 		while (binary_left_half[exponent_shift] != '1' && binary_left_half[exponent_shift] != '\0') {
 			exponent_shift++;
