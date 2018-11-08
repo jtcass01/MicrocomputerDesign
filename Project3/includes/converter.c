c8f96607908fc970fd9b8351e44b837cc8092230
Jacob Taylor Cassady <jtcass01@louisville.edu>
2018-11-08 00:49:15 -0500

    .

--- Project3/includes/converter.c
+++ Project3/includes/converter.c
@@ -321,7 +321,7 @@ uint32_t get_hex(char *sign, char *exponent, char *mantissa) {
 	for (int i = 22; i >= 0; i--) {
 		if (mantissa[i] == '1') {
 			result += (1 << two_power);
-			printf("1 found in mantissa @ i = %d, two_power = %d", i, two_power);
+			printf("1 found in mantissa @ i = %d, two_power = %d\n", i, two_power);
 		}
 		two_power++;
 	}
@@ -329,7 +329,7 @@ uint32_t get_hex(char *sign, char *exponent, char *mantissa) {
 	for (int i = 7; i >= 0; i--) {
 		if (exponent[i] == '1') {
 			result += (1 << two_power);
-			printf("1 found in exponent @ i = %d, two_power = %d", i, two_power);
+			printf("1 found in exponent @ i = %d, two_power = %d\n", i, two_power);
 		}
 		two_power++;
 	}
@@ -337,7 +337,7 @@ uint32_t get_hex(char *sign, char *exponent, char *mantissa) {
 	for (int i = 0; i >= 0; i--) {
 		if (sign[i] == '1') {
 			result += (1 << two_power);
-			printf("1 found in sign @ i = %d, two_power = %d", i, two_power);
+			printf("1 found in sign @ i = %d, two_power = %d\n", i, two_power);
 		}
 		two_power++;
 	}
